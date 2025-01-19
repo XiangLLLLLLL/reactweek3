195107ea947151e1895f46d2af360ed1aa6952cb
jhdpc <as313234@gmail.com>
2025-01-19 17:24:58 +0800

    路徑調整

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 開發中、產品路徑
+  base: process.env.NODE_ENV === "production" ? "/reactweek3/" : "/", // 加入此段程式碼
   plugins: [react()],
-})
+});
